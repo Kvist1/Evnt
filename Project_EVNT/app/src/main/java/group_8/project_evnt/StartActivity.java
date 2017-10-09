5ead06e686deade8bdf2fbd06d94a29da1e302a1
Heart Kittipon <heartmon12@gmail.com>
2017-10-09 14:47:25 +0200

    connect StartActivity with MainActivity

--- Project_EVNT/app/src/main/java/group_8/project_evnt/StartActivity.java
+++ Project_EVNT/app/src/main/java/group_8/project_evnt/StartActivity.java
@@ -1,7 +1,11 @@
 package group_8.project_evnt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,4 +14,16 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    public void onCreateButtonClick(View view) {
+        // TODO: Call to firebae to create a room and get the room id
+        Random random = new Random();
+        // generate a random integer from 0 to 999
+        Integer x = random.nextInt(999);
+        String roomId = x.toString();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(getString(R.string.key_room_id), roomId);
+        startActivity(intent);
+    }
+
 }
