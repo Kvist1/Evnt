45307de1cbd7868cdd8b103d9d353e995eca7b3d
Simon Lindkvist <simon_lindkvist92@hotmail.com>
2017-10-09 14:59:47 +0200

    Added pollactivity and fragment shell

---
+++ Project_EVNT/app/src/main/java/group_8/project_evnt/PollFragment.java
@@ -0,0 +1,23 @@
+package group_8.project_evnt;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PollFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.poll_fragment, container, false);
+
+
+        return rootView;
+    }
+}
