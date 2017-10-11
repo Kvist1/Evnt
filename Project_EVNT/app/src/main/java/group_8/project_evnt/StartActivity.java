5e57f78b07de4a88ac227f7d5cdc4d3b6b00c4b5
Felixfranzen <felixfranzn@gmail.com>
2017-10-11 13:27:22 +0200

    removed test code

--- Project_EVNT/app/src/main/java/group_8/project_evnt/StartActivity.java
+++ Project_EVNT/app/src/main/java/group_8/project_evnt/StartActivity.java
@@ -3,15 +3,6 @@ package group_8.project_evnt;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
-
-import group_8.project_evnt.core.Database;
-
 import android.view.View;
 
 import java.util.Random;
@@ -22,30 +13,6 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-        DatabaseReference chat = Database.getInstance().chat("MY_NEW_ROOM_ID");
-        chat.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.toString();
-
-                if (value != null){
-                    Log.i("LENGTH: ", String.valueOf(dataSnapshot.getChildrenCount()));
-                    Log.i("VALUES: ", value);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        Database.getInstance().writeChatMessage("MY_NEW_ROOM_ID", "Felix Franzén", "This is my new message");
-        String newRoomId = Database.getInstance().createRoom();
-        Log.i("THE NEW ROOM ID: ", newRoomId);
-        Database.getInstance().writeChatMessage(newRoomId, "Felix Franzén", "This is my new message");
-
     }
 
     public void onCreateButtonClick(View view) {
