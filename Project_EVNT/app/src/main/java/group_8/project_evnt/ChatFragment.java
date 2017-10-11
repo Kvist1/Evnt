3c2a9da333e29495cbeb74a8997cea82e65cf844
Heart Kittipon <heartmon12@gmail.com>
2017-10-11 15:15:54 +0200

    basic message layout and send message action

--- Project_EVNT/app/src/main/java/group_8/project_evnt/ChatFragment.java
+++ Project_EVNT/app/src/main/java/group_8/project_evnt/ChatFragment.java
@@ -1,12 +1,19 @@
 package group_8.project_evnt;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,6 +21,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import group_8.project_evnt.core.Database;
 import group_8.project_evnt.models.ChatMessage;
@@ -24,11 +32,15 @@ import group_8.project_evnt.models.ChatMessage;
  * Use the {@link ChatFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements View.OnClickListener  {
     private static final String ARG_ROOM_ID = "roomid";
     private String currentRoomId;
     private ArrayList<ChatMessage> chatMessages = new ArrayList<>();
 
+
+    private RecyclerView mChatMessageRecycleView;
+    private ImageButton mSendMessageButton;
+    private EditText mMessageInputEditText;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -52,6 +64,12 @@ public class ChatFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // dummy
+        ChatMessage c1 = new ChatMessage("1", "Hello, World");
+        ChatMessage c2 = new ChatMessage("2", "Hi :D");
+        chatMessages.add(c1);
+        chatMessages.add(c2);
+
         if (getArguments() != null) {
             currentRoomId = getArguments().getString(ARG_ROOM_ID);
         } else {
@@ -72,24 +90,118 @@ public class ChatFragment extends Fragment {
 
             }
         });
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        mChatMessageRecycleView = view.findViewById(R.id.rv_chat_message);
+        mSendMessageButton = view.findViewById(R.id.button_send_message);
+        //set onClick listener
+        mSendMessageButton.setOnClickListener(this);
+        mMessageInputEditText = view.findViewById(R.id.et_message_input);
+
+        return view;
     }
 
-    // when sending new message
-    public void onSendMessage(View view) {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup adapter
+        // Create adapter passing in the sample user data
+        ChatMessageAdapter adapter = new ChatMessageAdapter(this.getActivity(), chatMessages);
+        // Attach the adapter to the recyclerview to populate items
+        mChatMessageRecycleView.setAdapter(adapter);
+        // Set layout manager to position the items
+        mChatMessageRecycleView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
 
     }
 
-    // when getting new message
-    private void onReceiveMessage() {
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            // when sending new message
+            case R.id.button_send_message:
+                String msg = mMessageInputEditText.getText().toString();
+                Database.getInstance().writeChatMessage(currentRoomId, "111", msg);
 
+                mMessageInputEditText.setText("");
+//                mMessageInputEditText.clearFocus();
+                break;
+        }
+    }
+
+    // Create the basic adapter extending from RecyclerView.Adapter
+    // Note that we specify the custom ViewHolder which gives us access to our views
+    public class ChatMessageAdapter extends
+            RecyclerView.Adapter<ChatMessageAdapter.ViewHolder> {
+
+        private ArrayList<ChatMessage> mChatMessages;
+        private Context mContext;
+
+        // Pass in the contact array into the constructor
+        public ChatMessageAdapter(Context context, ArrayList<ChatMessage> chatMessages) {
+            mChatMessages = chatMessages;
+            mContext = context;
+        }
+
+        // Easy access to the context object in the recyclerview
+        private Context getContext() {
+            return mContext;
+        }
+
+        // Provide a direct reference to each of the views within a data item
+        // Used to cache the views within the item layout for fast access
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // Your holder should contain a member variable
+            // for any view that will be set as you render a row
+            public TextView mMessageTextView;
+            public TextView mSenderTextView;
+            public TextView mYouOrOtherTextView;
+
+            // We also create a constructor that accepts the entire item row
+            // and does the view lookups to find each subview
+            public ViewHolder(View itemView) {
+                // Stores the itemView in a public final member variable that can be used
+                // to access the context from any ViewHolder instance.
+                super(itemView);
+
+                mMessageTextView = (TextView) itemView.findViewById(R.id.tv_message);
+//                mSenderTextView = (TextView) itemView.findViewById(R.id.tv_sender);
+//                mYouOrOtherTextView = (TextView) itemView.findViewById(R.id.tv_you_or_other);
+            }
+        }
+
+        @Override
+        public ChatMessageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            // Inflate the custom layout
+            View yourMessageView = inflater.inflate(R.layout.chat_your_message_item, parent, false);
+
+            // Return a new holder instance
+            ViewHolder viewHolder = new ViewHolder(yourMessageView);
+            return viewHolder;
+        }
+
+        // Involves populating data into the item through holder
+        @Override
+        public void onBindViewHolder(ChatMessageAdapter.ViewHolder viewHolder, int position) {
+            // Get the data model based on position
+            ChatMessage chatMessage = mChatMessages.get(position);
+
+            // Set item views based on your views and data model
+            TextView message = viewHolder.mMessageTextView;
+            message.setText(chatMessage.getMessage());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mChatMessages.size();
+        }
     }
 
 }
