a0ff43d692ebd9731f89231e7e753775c90e52d9
Simon Lindkvist <simon_lindkvist92@hotmail.com>
2017-10-18 11:33:45 +0200

    removed the materials tab

--- Project_EVNT/app/src/main/java/group_8/project_evnt/MainActivity.java
+++ Project_EVNT/app/src/main/java/group_8/project_evnt/MainActivity.java
@@ -167,8 +167,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 2 total pages.
+            return 2;
         }
 
         @Override
@@ -178,8 +178,6 @@ public class MainActivity extends AppCompatActivity {
                     return "CHAT";
                 case 1:
                     return "POLLS";
-                case 2:
-                    return "MATERIALS";
             }
             return null;
         }
